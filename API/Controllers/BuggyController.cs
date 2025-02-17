ffd778c2939eb87fbc357b2b7d7fa7e27bb605db
oussama944 <oussamagsib@hotmail.fr>
2025-02-17 13:22:05 +0100

    add identity to the API with additional endpoints

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,4 +1,6 @@
+using System.Security.Claims;
 using Core.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -31,5 +33,16 @@ public class BuggyController : BaseApiController
     {
         return Ok();
     }
-    
+
+    [Authorize]
+    [HttpGet("secret")]
+    public IActionResult GetSecret()
+    {
+        var name = User.FindFirst(ClaimTypes.Name)?.Value;
+        var id = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        return Ok("Hello " + name + " with id " + id);
+    }
+
+
 }
